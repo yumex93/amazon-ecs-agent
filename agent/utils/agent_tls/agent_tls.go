390a392d54f607a371898a8c6ae0703be9ab6c66
yumex93 <yumex@amazon.com>
2018-07-11 14:56:04 -0700

    add httpclient test

--- agent/utils/agent_tls/agent_tls.go
+++ agent/utils/agent_tls/agent_tls.go
@@ -4,11 +4,8 @@ import (
 	"crypto/tls"
 )
 // Only support a subset of ciphers, corresponding cipher suite names can be found here: https://golang.org/pkg/crypto/tls/#Config
-var supportedCipherSuites = []uint16{0xc02f, 0xc027, 0xc013, 0xc030, 0xc014, 0x009c, 0x003c, 0x002f, 0x009d, 0x0035}
+var SupportedCipherSuites = []uint16{0xc02f, 0xc027, 0xc013, 0xc030, 0xc014, 0x009c, 0x003c, 0x002f, 0x009d, 0x0035}
 
 func SetCipherCuites(config *tls.Config) {
-	config.CipherSuites = supportedCipherSuites
+	config.CipherSuites = SupportedCipherSuites
 }
-
-
-
